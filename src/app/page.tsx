0bef35b07f58d33f36e395966a645fb9ed40b63d
Jaffar Gamer <jaffarkingsas@gmail.com>
2025-10-02 00:12:45 +0000

    import DotGrid from './DotGrid';
    <div style={{ width: '100%', height: '

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="relative flex min-h-screen w-full flex-col">
       <DotGrid
-        dotSize={5}
+        dotSize={2}
         gap={15}
         baseColor="#291d3b"
         activeColor="#00FFFF"
