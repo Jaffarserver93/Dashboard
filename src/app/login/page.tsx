d7bedc69d8faa2688273926c91ce93258ce789da
Jaffar Gamer <jaffarkingsas@gmail.com>
2025-10-03 13:35:09 +0000

    Fic background it showing black screen not same as verify page dot grid

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useState } from 'react';
-import { useRouter } from 'next/navigation';
 import { motion } from 'framer-motion';
 import { DiscordLogo } from '@/components/icons';
 import { Button } from '@/components/ui/button';
